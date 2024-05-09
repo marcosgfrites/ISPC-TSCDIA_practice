0f72f87af852f8bc9fdeaa08cc96038467eaba3c
Marcos Gabriel Frites Debenedetti <marcos.frites@bitlogic.io>
2024-05-08 22:18:12 -0300

    first commit

---
+++ 2024_semestre_01/programador/intro_programacion/3.3_variables_constantes_tipos_datos/ejemplos_practicos/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+B
+primer_programa.py,0\d\0d15d38f1a42fb77ee881d7b429106f4015ef623
---
+++ 2024_semestre_01/programador/intro_programacion/3.3_variables_constantes_tipos_datos/ejemplos_practicos/primer_programa.py
@@ -0,0 +1,29 @@
+# # # Ej. de demostración de la función print
+PI = 3.14159265359
+
+print(PI)
+
+radio = 5
+
+area_circunferencia = PI * radio ** 2  # ** es el operador de potencia
+
+print(area_circunferencia)
+print("El área de la circungerencia es: ", area_circunferencia)
+
+# # # Ej. de interacción con el usuario - input de texto
+nombre = input("¿Cómo te llamas? ")
+
+print("Hola {nombre}, ¿cómo estás?".format(nombre=nombre))
+
+# # # Ej. de interacción con el usuario - input de números
+numero1 = int(input("Dame un primer número: "))  # int() convierte el texto a un número entero
+numero2 = int(input("Dame un segundo número: "))  # si no agrego int() el input se toma como texto
+numero3 = int(input("Dame un tercer número: "))
+
+suma = numero1 + numero2 + numero3  # si el input es texto, la suma será una concatenación
+
+promedio = suma / 3
+
+print("La suma de los números es: ", suma)
+print("El promedio de los números es: ", promedio)
+
